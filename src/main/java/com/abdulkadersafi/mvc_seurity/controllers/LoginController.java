79ef963bb233317455247ebe8910b40f06bf7b6d
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2025-06-10 20:19:31 +0300

    Refactor login form layout and enhance error message display

--- src/main/java/com/abdulkadersafi/mvc_seurity/controllers/LoginController.java
+++ src/main/java/com/abdulkadersafi/mvc_seurity/controllers/LoginController.java
@@ -2,7 +2,6 @@ package com.abdulkadersafi.mvc_seurity.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,10 +11,4 @@ public class LoginController {
     public String loginForm() {
         return "login";
     }
-
-    @PostMapping("/login")
-    public String loginLogin() {
-        return "";
-    }
-
 }
