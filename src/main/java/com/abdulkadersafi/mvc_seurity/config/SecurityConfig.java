23f5468a9c8cc6de2c6e9660dcb416e4fccadb58
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2025-06-10 21:07:33 +0300

    Refactor user details management to use JdbcUserDetailsManager for database integration

--- src/main/java/com/abdulkadersafi/mvc_seurity/config/SecurityConfig.java
+++ src/main/java/com/abdulkadersafi/mvc_seurity/config/SecurityConfig.java
@@ -1,38 +1,44 @@
 package com.abdulkadersafi.mvc_seurity.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+  // @Bean
+  // public InMemoryUserDetailsManager userDetailsManager() {
+  // UserDetails admin = User.builder()
+  // .username("admin")
+  // .password("{noop}admin")
+  // .roles("ADMIN", "MANAGER", "EMPLOYEE")
+  // .build();
+
+  // UserDetails manager = User.builder()
+  // .username("manager")
+  // .password("{noop}manager")
+  // .roles("MANAGER", "EMPLOYEE")
+  // .build();
+
+  // UserDetails employee = User.builder()
+  // .username("employee")
+  // .password("{noop}employee")
+  // .roles("EMPLOYEE")
+  // .build();
+
+  // return new InMemoryUserDetailsManager(admin, manager, employee);
+  // }
+
   @Bean
-  public InMemoryUserDetailsManager userDetailsManager() {
-
-    UserDetails admin = User.builder()
-        .username("admin")
-        .password("{noop}admin")
-        .roles("ADMIN", "MANAGER", "EMPLOYEE")
-        .build();
-
-    UserDetails manager = User.builder()
-        .username("manager")
-        .password("{noop}manager")
-        .roles("MANAGER", "EMPLOYEE")
-        .build();
-
-    UserDetails employee = User.builder()
-        .username("employee")
-        .password("{noop}employee")
-        .roles("EMPLOYEE")
-        .build();
-
-    return new InMemoryUserDetailsManager(admin, manager, employee);
+  public UserDetailsManager userDetailsManager(DataSource dataSource) {
+    // get the user and roles from the data base using users and authorities tables
+    return new JdbcUserDetailsManager(dataSource);
   }
 
   @Bean
